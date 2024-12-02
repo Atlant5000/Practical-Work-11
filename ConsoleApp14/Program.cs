d2eb7ccfb19ad0ce947d45e2844ac75d27cda430
Atlant5000 <maximmmd13@gmail.com>
2024-12-02 19:46:45 +0200

    Task 5 Hw

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+        public static void Input()
+        {
+            Conclusion($"y=", Math.Round(Func(7) + Func(11) + Func(15), 2));
+            Console.ReadKey();
+        }
+        public static double Func(int a)
+        {
+            return (Math.Sqrt(a) + a) / 3;
+        }
+        public static void Conclusion(string message, double max)
+        {
+            Console.WriteLine($"{message} {max}");
+        }
+    }
+}
