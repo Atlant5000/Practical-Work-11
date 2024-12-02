2a5831a70ef3f80d4abb4c9b50e30d4cb1e20e1f
Atlant5000 <maximmmd13@gmail.com>
2024-12-02 19:34:21 +0200

    Task 5 Cw

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Input();
+        }
+        public static void Input()
+        {
+            Conclusion($"x=",Math.Round(Func(6) + Func(13) + Func(21), 2));
+            Console.ReadKey();
+        }
+        public static double Func(int a)
+        {
+            return (Math.Sqrt(a) + a) / 2;
+        }
+        public static void Conclusion(string message, double max)
+        {
+            Console.WriteLine($"{message} {max}");
+        }
+    }
+}
